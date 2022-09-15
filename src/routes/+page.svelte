41f18145dbafc59d41bf0f8c68578905eeb39254
edde746 <86283021+edde746@users.noreply.github.com>
2022-09-15 21:11:06 +0200

    Performance Fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { ArrowRightIconSolid } from '@codewithshin/svelte-heroicons';
   import { dragScroll } from '$lib/dragScroll';
+  import Project from './Project.svelte';
 </script>
 
 <div class="max-w-3xl mx-auto px-4 py-8 grid gap-8">
@@ -75,48 +76,22 @@
       <p class="text-sm">Unity games made with Unity and C#. Playable in browser powered by WASM.</p>
     </div>
     <div class="projects snap-x" use:dragScroll>
-      <a class="relative group" rel="external" href="https://github.com/edde746/survival-spel/">
-        <div class="project-overlay">
-          <h3>survival-spel</h3>
-          <p>
-            Survival game built in Unity, has procedural map generation, resources gathering, building, inventory and
-            crafting system.<br />Not playable in browser.
-          </p>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <video class="w-full" autoplay loop muted playsinline>
-          <source src="/images/survival.mp4" type="video/mp4" />
-        </video>
-      </a>
-      <a class="relative group" rel="external" href="https://edde746.github.io/rpg-spel/">
-        <div class="project-overlay">
-          <h3>rpg-spel</h3>
-          <p>Basic RPG mechanics like movement and a dialogue system.</p>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/rpg.gif" alt="RPG Spel" />
-      </a>
-      <a class="relative group" rel="external" href="https://edde746.github.io/fps-spel/">
-        <div class="project-overlay">
-          <h3>fps-spel</h3>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/fps.gif" alt="FPS Spel" />
-      </a>
-      <a class="relative group" rel="external" href="https://edde746.github.io/fysikspel/">
-        <div class="project-overlay">
-          <h3>fysikspel</h3>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/fysik.gif" alt="Fysikspel" />
-      </a>
-      <a class="relative group" rel="external" href="https://edde746.github.io/plattformspel/">
-        <div class="project-overlay">
-          <h3>plattformspel</h3>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/plattform.gif" alt="Plattformspel" />
-      </a>
+      <Project title="Survival Game" href="https://github.com/edde746/survival-spel/" video="survival.mp4">
+        Survival game built in Unity, has procedural map generation, resources gathering, building, inventory and
+        crafting system.<br />Not playable in browser.
+      </Project>
+      <Project title="RPG Game" href="https://github.com/edde746/rpg-spel/" video="rpg.mp4">
+        Basic RPG mechanics like movement and a dialogue system.
+      </Project>
+      <Project title="FPS Game" href="https://github.com/edde746/fps-spel/" video="fps.mp4">
+        First person shooter game with basic movement and shooting mechanics, time trial mode.
+      </Project>
+      <Project title="Angry Brids Clone" href="https://github.com/edde746/fysikspel/" video="fysik.mp4">
+        Physics based game with a slingshot mechanic. Trajectory preview and physics based movement.
+      </Project>
+      <Project title="Platformer" href="https://github.com/edde746/plattformspel/" video="plattform.mp4">
+        Platformer game with basic movement, enemies and a few levels.
+      </Project>
     </div>
   </div>
 
@@ -128,98 +103,40 @@
       </p>
     </div>
     <div class="projects snap-x" use:dragScroll>
-      <a class="relative group" rel="external" href="https://divinemods.com">
-        <div class="project-overlay">
-          <h3>Divine</h3>
-          <p>
-            E-commerce site for software licenses built with SvelteKit, Strapi and Docker.<br />
-            Integrates Stripe, Coinbase and PayPal for payments and Amazon SES for email including magic-link login.
-          </p>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <video class="w-full" autoplay loop muted playsinline>
-          <source src="/images/divine.mp4" type="video/mp4" />
-        </video>
-      </a>
-      <a class="relative group" rel="external" href="https://zeropointexotics.com">
-        <div class="project-overlay">
-          <h3>ZeroPoint Exotics</h3>
-          <p>
-            Rental car booking site built with SvelteKit, Strapi and Docker.<br />
-            Renders contacts as docx and e-mails them. Cars can be managed via the Strapi admin panel.
-          </p>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <video class="w-full" autoplay loop muted playsinline>
-          <source src="/images/zp.mp4" type="video/mp4" />
-        </video>
-      </a>
-      <a class="relative group" rel="external" href="https://cedara.cc">
-        <div class="project-overlay">
-          <h3>Cedara</h3>
-          <p>
-            E-commerce site for software licenses built with SvelteKit, Strapi and Docker.<br />
-            Uses Stripe and Coinbase for payments and integrated OAuth login with Discord and Google.
-          </p>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <video class="w-full" autoplay loop muted playsinline>
-          <source src="/images/cedara.mp4" type="video/mp4" />
-        </video>
-      </a>
-      <a class="relative group" rel="external" href="https://github.com/edde746/twitter-clone">
-        <div class="project-overlay">
-          <h3>twitter-clone</h3>
-          <p>
-            Twitter clone built with SvelteKit and Redis.<br />Uses Redis as the primary database and Amazon S3 for user
-            uploads such as images.
-          </p>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/twitter.gif" alt="Twitter Clone" />
-      </a>
-      <a class="relative group" rel="external" href="https://tiktok-downloader-ebon.vercel.app/">
-        <div class="project-overlay">
-          <h3>tiktok-downloader</h3>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/tiktok.gif" alt="Tiktok Downloader" />
-      </a>
-      <a class="relative group" rel="external" href="https://github.com/edde746/live-chat">
-        <div class="project-overlay">
-          <h3>live-chat</h3>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/live-chat.gif" alt="Live Chat" />
-      </a>
-      <a class="relative group" rel="external" href="https://edde746.github.io/rdpservers/">
-        <div class="project-overlay">
-          <h3 style="color:#fff">rdpservers</h3>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/rdp.gif" alt="rdpservers" />
-      </a>
-      <a class="relative group" rel="external" href="https://edde746.github.io/nft-gen/">
-        <div class="project-overlay">
-          <h3>nft-gen</h3>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/nft.gif" alt="NFT Generator" />
-      </a>
-      <a class="relative group" rel="external" href="https://edde746.github.io/svelte-toasts/">
-        <div class="project-overlay">
-          <h3>toasts demo</h3>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/toasts.gif" alt="svelte-toasts" />
-      </a>
-      <a class="relative group" rel="external" href="https://gymnasieprojekt-2021.herokuapp.com/">
-        <div class="project-overlay">
-          <h3 style="color:#fff">gymnasieprojekt</h3>
-          <ArrowRightIconSolid className="bottom-arrow" />
-        </div>
-        <img src="/images/gymnasieprojekt.gif" alt="gymnasieprojekt" />
-      </a>
+      <Project title="Divine" href="https://divinemods.com" video="divine.mp4">
+        E-commerce site for software licenses built with SvelteKit, Strapi and Docker.<br />
+        Integrates Stripe, Coinbase and PayPal for payments and Amazon SES for email including magic-link login.
+      </Project>
+      <Project title="ZeroPoint Exotics" href="https://zeropointexotics.com" video="zp.mp4">
+        Rental car booking site built with SvelteKit, Strapi and Docker.<br />
+        Renders contacts as docx and e-mails them. Cars can be managed via the Strapi admin panel.
+      </Project>
+      <Project title="Cedara" href="https://cedara.cc" video="cedara.mp4">
+        E-commerce site for software licenses built with SvelteKit, Strapi and Docker.<br />
+        Uses Stripe and Coinbase for payments and integrated OAuth login with Discord and Google.
+      </Project>
+      <Project title="Twitter Clone" href="https://github.com/edde746/twitter-clone" video="twitter.mp4">
+        Twitter clone built with SvelteKit and Redis.<br />
+        Uses Redis as the primary database and Amazon S3 for user uploads such as images.
+      </Project>
+      <Project title="TikTok Downloader" href="https://tiktok-downloader-ebon.vercel.app/" video="tiktok.mp4">
+        Most likely broken due to API changes.
+      </Project>
+      <Project title="Live Chat" href="https://github.com/edde746/live-chat" video="live-chat.mp4">
+        Very basic live chat built with Next.JS and Socket.IO.
+      </Project>
+      <Project title="RDPservers" href="https://edde746.github.io/rdpservers/" video="rdp.mp4">
+        Final project for a course in web development. Built in Next.JS, no backend.
+      </Project>
+      <Project title="NFT Generator" href="https://edde746.github.io/nft-gen/" video="nft.mp4">
+        Generate NFT like images from a set of components, uses HTML5 canvas.
+      </Project>
+      <Project title="Svelte Toasts Demo" href="https://edde746.github.io/svelte-toasts/" video="toasts.mp4">
+        Alternative demo page for the Svelte Toasts library.
+      </Project>
+      <Project title="Gymnasieprojekt" href="https://github.com/edde746/gymnasieprojekt" video="gymnasieprojekt.mp4">
+        Gymnasieprojekt 2021, built with Django and React.
+      </Project>
     </div>
   </div>
 
---
+++ src/routes/Project.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { ArrowRightIconSolid } from '@codewithshin/svelte-heroicons';
+  import { inview } from 'svelte-inview';
+
+  export let title;
+  export let href;
+  export let video;
+  let loadVideo = false;
+</script>
+
+<a class="relative group" rel="external" {href} target="_blank" use:inview on:enter={() => (loadVideo = true)}>
+  <div class="project-overlay">
+    <h3>{title}</h3>
+    <p>
+      <slot />
+    </p>
+    <ArrowRightIconSolid className="bottom-arrow" />
+  </div>
+  {#if video && loadVideo}
+    <video class="w-full" autoplay loop muted playsinline>
+      <source src="/projects/{video}" type="video/mp4" />
+    </video>
+  {/if}
+</a>
